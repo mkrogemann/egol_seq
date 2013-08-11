04965cb9774d0e90acc46c489f163fe14f4cf5e7
Markus Krogemann <markus@krogemann.de>
2013-08-11 21:20:03 +0200

    improve documentation

--- src/rules.erl
+++ src/rules.erl
@@ -2,9 +2,14 @@
 
 -export ([evaluate/2]).
 
-% evaluate expects two arguments:
-% 1) the number of living neighbors
-% 2) whether or not the cell evaluated is currently alive
+%%--------------------------------------------------------------------
+%% @doc
+%% evaluate expects two arguments:
+%% 1) the number of living neighbors
+%% 2) whether or not the cell evaluated is currently alive
+%% @spec evaluate( integer(), true | false ) -> true | false
+%% @end
+%%--------------------------------------------------------------------
 evaluate(2, true) -> true;
 evaluate(3, _) -> true;
 evaluate(_, _) -> false.
