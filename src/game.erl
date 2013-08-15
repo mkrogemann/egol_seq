3de0e5c69962d4df2f7482f920e9a397b7f8d74c
Markus Krogemann <markus@krogemann.de>
2013-08-16 00:42:18 +0200

    implement state and neighbors

--- src/game.erl
+++ src/game.erl
@@ -15,18 +15,35 @@ evolve(Grid) ->
   dict:map(fun(K,V) ->
     rules:evaluate(neighbors(K, Grid), alive(V)) end, Grid).
 
-%%
-%%
-%%
+%%--------------------------------------------------------------------
+%% @doc
+%% alive takes one argument which represents a Cell's status
+%% A value of 1 means alive, everything else means not alive
+%% @spec alive( integer() ) -> true | false
+%% @end
+%%--------------------------------------------------------------------
 alive(Status) when Status =:= 1 -> true;
 alive(_) -> false.
 
 %%
 %%
 %%
-neighbors(K, Grid) ->
-  % K is a point (tuple)
-  2.
+neighbors(Cell, Grid) ->
+  {X,Y} = Cell,
+  lists:sum(
+    [state({X-1,Y-1}, Grid), state({X-1,Y}, Grid), state({X-1,Y+1}, Grid),
+     state({X,Y-1}, Grid), state({X,Y+1}, Grid),
+     state({X+1,Y-1}, Grid), state({X+1,Y}, Grid), state({X+1,Y+1}, Grid)]).
 
 
-% the dict has tubple X,Y as key and status as value
+%%--------------------------------------------------------------------
+%% @doc
+%% state takes two arguments:
+%% 1) the cell to find (a tuple of X,Y coordinates)
+%% 2) the grid in which to find the cell
+%% @spec alive( { integer(), integer() }, dict ) -> 1 | 0
+%% @end
+%%--------------------------------------------------------------------
+state(Cell, Grid) ->
+  {ok,Value} = dict:find(Cell, Grid),
+  Value.
--- test/game_tests.erl
+++ test/game_tests.erl
@@ -5,3 +5,20 @@ alive_test() ->
   ?assert(game:alive(1)),
   ?assertNot(game:alive(0)).
 
+neighbors_test() ->
+  Dict = dict:from_list([ {{1,1},1}, {{1,2},0}, {{1,3},1}, {{1,4},0},
+                          {{2,1},0}, {{2,2},1}, {{2,3},1}, {{2,4},0},
+                          {{3,1},0}, {{3,2},0}, {{3,3},1}, {{3,4},0},
+                          {{4,1},1}, {{4,2},0}, {{4,3},1}, {{4,4},0} ]),
+
+  ?assertEqual(4, game:neighbors({2,2}, Dict)),
+  ?assertEqual(3, game:neighbors({2,3}, Dict)),
+  ?assertEqual(5, game:neighbors({3,2}, Dict)),
+  ?assertEqual(3, game:neighbors({3,3}, Dict)).
+
+state_test() ->
+  Dict = dict:from_list([{{1,1},1},{{1,2},0}]),
+  ?assertEqual(1, game:state({1,1}, Dict)),
+  ?assertEqual(0, game:state({1,2}, Dict)).
+
+
