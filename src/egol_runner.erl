d3a15d308a0ecba0fbe72497c5cdd9f53bc3f373
Markus Krogemann <markus@krogemann.de>
2014-03-24 01:06:31 +0100

    rather than use a generator, just zip the two lists

--- src/egol_runner.erl
+++ src/egol_runner.erl
@@ -5,15 +5,17 @@
 random_start(Width, Height) ->
     random:seed(os:timestamp()),
     CellStates = [random:uniform(2) - 1 || _ <- lists:seq(1, Width*Height)],
-    BoardAsList = [{{X,Y},Z} || X <- lists:seq(1, Width),
-                                Y <- lists:seq(1, Height),
-                                Z <- CellStates],
+    CoordsList = [{X,Y} || X <- lists:seq(1, Width), Y <- lists:seq(1, Height)],
+    BoardAsList = lists:zip(CoordsList, CellStates),
     egol_game:init_from_list(BoardAsList).
 
 
-next_gen(_Board, Generation, MaxGenerations) when MaxGenerations < Generation ->
-    ok;
+next_gen(_Board, Generation, MaxGenerations) when MaxGenerations < Generation -> ok;
 next_gen(Board, Generation, MaxGenerations) ->
     NextBoard = egol_game:evolve(Board),
+    timer:sleep(50),
+    io:format("\e[2J"),
+    io:format("Generation ~w~n", [Generation]),
+    egol_print:print_game(NextBoard),
     next_gen(NextBoard, Generation+1, MaxGenerations).
 
